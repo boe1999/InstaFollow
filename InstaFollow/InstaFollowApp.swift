c5c78e2e5737d2360fc9171c10669937365469d9
Boe <boerio99@gmail.com>
2023-03-14 11:14:53 +0100

    Initial Commit

---
+++ InstaFollow/InstaFollowApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFollowApp.swift
+//  InstaFollow
+//
+//  Created by Daniele Boerio on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFollowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
